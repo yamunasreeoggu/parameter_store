9a26b9565faaf0b9b7d0bdb6f12cfd980882dcf9
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-08 19:14:16 -0400

    creating tg

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_ssm_parameter" "params" {
 variable "parameters" {
   default = [
     {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"},
-    {name = "prod.rds.master_username", value = "expense", type = "String"},
+    {name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String"},
     {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.yamunadevops.online/", type = "String"},
     {name = "prod.rds.endpoint", value = "prod-mysql.cluster-ci0wnnvfomah.us-east-1.rds.amazonaws.com", type = "String"},
 
