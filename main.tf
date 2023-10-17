b4a383f97413fe997f6846b09bf97bab2437081c
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-17 18:37:21 -0400

    updated backend jenkinsfile

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ variable "parameters" {
     {name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String"},
     {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.yamunadevops.online/", type = "String"},
     {name = "prod.rds.endpoint", value = "prod-mysql.cluster-ci0wnnvfomah.us-east-1.rds.amazonaws.com", type = "String"},
+    {name = "prod.backend.app_version", value = "1.0.0", type = "String"},
+    {name = "prod.frontend.app_version", value = "1.0.0", type = "String"},
 
     # Dev Env
     {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.yamunadevops.online/", type = "String"},
