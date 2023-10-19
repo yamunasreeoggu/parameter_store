64909f3ff058cf2ce6e174f50d740edfdd99c47f
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-19 16:12:30 -0400

    adding Jenkins mutable file

--- main.tf
+++ main.tf
@@ -21,7 +21,9 @@ variable "parameters" {
     # Common env
     {name = "jenkins_password", value = "admin123", type = "SecureString"},
     {name = "artifactory_username", value = "admin", type = "String"},
-    {name = "artifactory_password", value = "Admin123", type = "SecureString"}
+    {name = "artifactory_password", value = "Admin123", type = "SecureString"},
+    {name = "ssh_username", value = "centos", type = "String"},
+    {name = "ssh_password", value = "DevOps321", type = "SecureString"},
   ]
 }
 
