d9e632e36a64d1282043ba9a97d5da5ce2fa0d66
yamunasreeoggu <yamunasree321@gmail.com>
2023-11-15 18:42:33 -0500

    D75 | Yamuna| create ansible structure and added code for frontend

--- main.tf
+++ main.tf
@@ -9,15 +9,15 @@ resource "aws_ssm_parameter" "params" {
 
 variable "parameters" {
   default = [
-    {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"},
-    {name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String"},
-    {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.yamunadevops.online/", type = "String"},
-    {name = "prod.rds.endpoint", value = "prod-mysql.cluster-ci0wnnvfomah.us-east-1.rds.amazonaws.com", type = "String"},
-    {name = "prod.backend.app_version", value = "1.0.0", type = "String"},
-    {name = "prod.frontend.app_version", value = "1.0.1", type = "String"},
-
-    # Dev Env
-    {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.yamunadevops.online/", type = "String"},
+#    {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"},
+#    {name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String"},
+#    {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.yamunadevops.online/", type = "String"},
+#    {name = "prod.rds.endpoint", value = "prod-mysql.cluster-ci0wnnvfomah.us-east-1.rds.amazonaws.com", type = "String"},
+#    {name = "prod.backend.app_version", value = "1.0.0", type = "String"},
+#    {name = "prod.frontend.app_version", value = "1.0.1", type = "String"},
+#
+#    # Dev Env
+#    {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.yamunadevops.online/", type = "String"},
 
     # Common env
     {name = "jenkins_password", value = "admin123", type = "SecureString"},
@@ -25,6 +25,13 @@ variable "parameters" {
     {name = "artifactory_password", value = "Admin123", type = "SecureString"},
     {name = "ssh_username", value = "centos", type = "String"},
     {name = "ssh_password", value = "DevOps321", type = "SecureString"},
+
+    # Roboshop Dev Env Parameters
+    {name = "dev.roboshop.frontend.catalogue_url", value = "http://catalogue-dev.yamunadevops.online:8080/", type = "String"},
+    {name = "dev.roboshop.frontend.cart_url", value = "http://cart-dev.yamunadevops.online:8080/", type = "String"},
+    {name = "dev.roboshop.frontend.user_url", value = "http://user-dev.yamunadevops.online:8080/", type = "String"},
+    {name = "dev.roboshop.frontend.shipping_url", value = "http://shipping-dev.yamunadevops.online:8080/", type = "String"},
+    {name = "dev.roboshop.frontend.payment_url", value = "http://payment-dev.yamunadevops.online:8080/", type = "String"},
   ]
 }
 
