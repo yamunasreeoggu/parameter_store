9415784dafb5222765f7d8fe8b9335565a90d102
yamunasreeoggu <yamunasree321@gmail.com>
2023-11-17 14:38:04 -0500

    D75 | Yamuna| added parameter for dev.roboshop.user.MONGO_ENDPOINT

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ variable "parameters" {
     {name = "dev.roboshop.frontend.payment_url", value = "http://payment-dev.yamunadevops.online:8080/", type = "String"},
     {name = "dev.roboshop.catalogue.MONGO_URL", value = "mongodb://mongodb-dev.yamunadevops.online:27017/catalogue", type = "String"},
     {name = "dev.roboshop.catalogue.MONGO_ENDPOINT", value = "mongodb-dev.yamunadevops.online", type = "String"},
+    {name = "dev.roboshop.user.MONGO_ENDPOINT", value = "mongodb-dev.yamunadevops.online", type = "String"},
     {name = "dev.roboshop.user.MONGO_URL", value = "mongodb://mongodb-dev.yamunadevops.online:27017/users", type = "String"},
     {name = "dev.roboshop.user.REDIS_HOST", value = "redis-dev.yamunadevops.online", type = "String"},
     {name = "dev.roboshop.cart.REDIS_HOST", value = "redis-dev.yamunadevops.online", type = "String"},
